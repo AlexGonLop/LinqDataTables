750ce4866449bea408cfd094bcae02b18bad4569
Alejandro Gonzalez <alejandro.gonzalez@onetsystems.mx>
2025-04-27 23:46:41 -0700

    Added Orderable Filters

---
+++ LinqDataTables/Linq/LinqDataTable.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq;
+using LinqDataTables.Generic;
+using LinqDataTables.Models;
+
+namespace LinqDataTables.Linq
+{
+    public static class LinqDataTable
+    {
+        public static DataTableResponse<TSource> ToDataTable<TSource>
+        (
+            this IQueryable<TSource> source,
+            DataTableRequest request
+        )
+        {
+            var total = source.Count();
+            var mainSource = source;
+
+            if (request.Order.Length > 0)
+                mainSource = ApplyOrder(mainSource, request.Order);
+
+            var totalFiltered = total;//mainSource.Count();
+            if (request.Start > 0)
+                mainSource = mainSource.Skip(request.Start);
+            if (request.Length > 0)
+                mainSource = mainSource.Take(request.Length);
+
+            var result = mainSource.ToList();
+
+            return new DataTableResponse<TSource>
+            {
+                Draw = request.Draw,
+                RecordsTotal = total,
+                RecordsFiltered = totalFiltered,
+                Data = result,
+            };
+        }
+
+        private static IQueryable<TSource> ApplyOrder<TSource>
+        (
+            IQueryable<TSource> source,
+            DataTableOrder[] order
+        )
+        {
+            IOrderedQueryable<TSource> ordered = null;
+            for (var i = 0; i < order.Length; i++)
+            {
+                var item = order[i];
+                var isAscending = item.Dir.Equals("ASC", StringComparison.InvariantCultureIgnoreCase);
+
+                ordered = i == 0 ? source.OrderBy(item.Name, isAscending) : ordered.ThenBy(item.Name, isAscending);
+            }
+
+            return ordered;
+        }
+    }
+}
---
+++ LinqDataTables/Linq/LinqExtensions.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace LinqDataTables.Linq
+{
+    internal static class LinqExtensions
+    {
+        public static IOrderedQueryable<TSource> OrderBy<TSource>
+        (
+            this IQueryable<TSource> source,
+            string field,
+            bool ascending = false
+        )
+        {
+            if (source is null)
+                throw new ArgumentNullException("source is null");
+            if (string.IsNullOrEmpty(field?.Trim()))
+                throw new ArgumentException("Field is null or empty");
+
+            var type = typeof(TSource);
+            var property = type.GetProperty(field) ?? throw new InvalidDataException(field);
+            var param = Expression.Parameter(type, "O");
+            var propAccess = Expression.MakeMemberAccess(param, property);
+            var lamdaExp = Expression.Lambda(propAccess, param);
+
+            var argsTypes = new Type[] { type, property.PropertyType };
+            var exp = Expression.Call(
+                type: typeof(Queryable),
+                methodName: ascending ? "OrderBy" : "OrderByDescending",
+                argsTypes,
+                source.Expression,
+                Expression.Quote(lamdaExp)
+            );
+
+            return (IOrderedQueryable<TSource>)source.Provider.CreateQuery<TSource>(exp);
+        }
+
+        public static IOrderedQueryable<TSource> ThenBy<TSource>
+        (
+            this IOrderedQueryable<TSource> source,
+            string field,
+            bool ascending = false
+        )
+        {
+            if (source is null)
+                throw new ArgumentNullException("source is null");
+            if (string.IsNullOrEmpty(field?.Trim()))
+                throw new ArgumentException("Field is null or empty");
+
+            var type = typeof(TSource);
+            var property = type.GetProperty(field) ?? throw new InvalidDataException(field);
+            var param = Expression.Parameter(type, "O");
+            var propAccess = Expression.MakeMemberAccess(param, property);
+            var lamdaExp = Expression.Lambda(propAccess, param);
+
+            var argsTypes = new Type[] { type, property.PropertyType };
+            var exp = Expression.Call(
+                type: typeof(Queryable),
+                methodName: ascending ? "ThenBy" : "ThenByDescending",
+                argsTypes,
+                source.Expression,
+                Expression.Quote(lamdaExp)
+            );
+
+            return (IOrderedQueryable<TSource>)source.Provider.CreateQuery<TSource>(exp);
+        }
+    }
+}
